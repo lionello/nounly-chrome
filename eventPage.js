c8ae8a8462eb1c02868e7f55f3f935083ddcb522
Lionello Lunesu <lio@lunesu.com>
2014-03-24 14:44:20 +0800

    Added Stinkybad to right-click context menu on links

--- eventPage.js
+++ eventPage.js
@@ -8,3 +8,11 @@ chrome.omnibox.onInputEntered.addListener(function (text, disposition) {
         chrome.tabs.create({ url: "http://stinkybad.com/?" + encodeURIComponent(text) });
     }
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  chrome.tabs.create({ url: "http://stinkybad.com/?" + encodeURIComponent(info.linkUrl) });
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({ id: "sb0", title: "Stinkybad this link", contexts: ["link"], targetUrlPatterns: ["http://*/*", "https://*/*"] });
+});
